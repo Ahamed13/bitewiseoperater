c7ec3f46a677851765f0521c72ad69813b5cf5f3
Ahamed Sheak Ansari S <80703598+Ahamed13@users.noreply.github.com>
2021-12-15 08:42:50 +0530

    Add files via upload

---
+++ Bitewiseoperator.java
@@ -0,0 +1,23 @@
+package bitewiseoperater;
+
+public class Bitewiseoperator {
+	
+	public static void main(String a[])
+	{
+		int b, c, d, e;
+		
+		b = 25;
+		c = 15;
+		
+		d = b & c; //--> AND operator
+		
+		e = b | c; //--> OR operator
+		
+		System.out.println("The AND operator result " + d);
+		System.out.println("The OR operator result "+ e);
+		
+		
+		
+	}
+
+}
